2748eaec092fbe0d9802d37e499f7fd355d9fd97
QSWZQSN <16222447567@qq.com>
2020-04-13 14:54:23 +0800

    基础语法(上)

---
+++ MethodTest.java
@@ -0,0 +1,8 @@
+public  class MethodTest {
+    private static void methodDemo() {
+        System.out.println("Hello Shiyanlou");
+    }
+    public static void main(String[] args) {
+        methodDemo();
+    }
+}
